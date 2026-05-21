abf9025f8fe260588060752ab0b33a35bebb8823
opvtyo <opvtyo45@gmail.com>
2026-05-21 21:16:00 +0900

    엔티티 파일 받은 후 코딩

--- Monster/Entity.cpp
+++ Monster/Entity.cpp
@@ -7,7 +7,7 @@ Entity::Entity(std::string name, int health, int attack)
 {
     this->name = name;
     this->health = health;
-    this->maxHealth = health;
+	this->maxHealth = health; // 현재는 안쓰지만 기믹 추가 할때를 대비해서 적어두었습니다. ex) 체력 회복 아이템, 몬스터가 체력을 회복하는 패턴 등
     this->attack = attack;
 }
 
