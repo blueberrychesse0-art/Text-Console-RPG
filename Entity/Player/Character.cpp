bcb7d4612f6ed27aba8d4fa4905aee04812324da
wns6677 <wns96096@gmail.com>
2026-05-22 15:55:09 +0900

    캐릭터 골드,엔티티 체력 공격력  get/set 추가

--- Entity/Player/Character.cpp
+++ Entity/Player/Character.cpp
@@ -25,6 +25,20 @@ void Character::DisplayStatus() const
 	cout << "체력 : " << maxHealth << "/" << health << " | 공격력 : " << attack << endl
 		<< "현재 레벨 : " << level << " | 경험치 : " << experience << "/100" << endl;
 }
+
+int Character::GetGold()const
+{
+	return gold;
+}
+void Character::AddGold(int amount)
+{
+	gold += amount;
+}
+void Character::UseGold(int amount)
+{
+	gold -= amount;
+}
+
 //int main() {
 //	string temp;
 //	cout << "이름을 입력하세요 : ";
--- Entity/Player/Character.h
+++ Entity/Player/Character.h
@@ -13,8 +13,16 @@ private:
 	std::vector<Item*> inventory;
 public:
 	Character(std::string n);
+
 	void Attack() override;
 	void TakeDamage(int damage) override;
 	void DisplayStatus()const;
+	
+	
+	int GetGold()const;
+
+	void AddGold(int amount);
+	void UseGold(int amount);
+
 };
 
